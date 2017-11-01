fb129193e0362d1e2a816ca5beae619503442970
TinaBreaton <tbreaton@outlook.com>
2017-11-01 17:20:08 -0400

    Added a function to get the new quote so it is called initially when the program starts.

--- quote-generator.js
+++ quote-generator.js
@@ -1,38 +1,43 @@
 //The call to the API will return 1 random quote from a famous movie or person in JSON format.
 
-var newColor = "";
+getNewQuote();
 
 $(document).ready(function() {
     $(".quote-button").click(function() {
-        //alert("Hello");
-         var output = $.ajax({
-            url: 'https://andruxnet-random-famous-quotes.p.mashape.com', // The URL to the API. You can get this in the API page of the API you intend to consume
-            type: 'GET', // The HTTP Method, can be GET POST PUT DELETE etc
-            data: {}, // Additional parameters here
-            dataType: 'json',
-            success: function(data) { 
-                console.log(data);        
-                
-                newColor = randomColor();
-                console.log(newColor);
-                //$("body, .quote-button").css({"background-color": newColor, "color": newColor}); 
-                $("body").css("background-color",  newColor); 
-                $(".quote-button").css("background-color",  newColor); 
-                $(".quote-box").css("color",  newColor); 
-                $(".fa-twitter").css("background-color", newColor);    
-                   
-                
-                document.getElementById("quote").innerHTML = '" ' + data.quote + ' "';
-                document.getElementById("author").innerHTML = "--  " + data.author;
-            },
-            error: function(err) { alert(err); },
-            beforeSend: function(xhr) {
-                xhr.setRequestHeader("X-Mashape-Authorization", "l75xvt8EmDmshqIBu665D14qnTOZp1FiQHDjsnm7HimMyWWOIS"); // Enter here your Mashape key
-            }
-        }); 
-
+        getNewQuote();
     }) 
 });
+
+function getNewQuote() {
+
+    var newColor = "";
+
+    var output = $.ajax({
+        url: 'https://andruxnet-random-famous-quotes.p.mashape.com', // The URL to the API. You can get this in the API page of the API you intend to consume
+        type: 'GET', // The HTTP Method, can be GET POST PUT DELETE etc
+        data: {}, // Additional parameters here
+        dataType: 'json',
+        success: function(data) { 
+            console.log(data);        
+            
+            newColor = randomColor();
+            console.log(newColor);
+            //$("body, .quote-button").css({"background-color": newColor, "color": newColor}); 
+            $("body").css("background-color",  newColor); 
+            $(".quote-button").css("background-color",  newColor); 
+            $(".quote-box").css("color",  newColor); 
+            $(".fa-twitter").css("background-color", newColor);    
+               
+            
+            document.getElementById("quote").innerHTML = '" ' + data.quote + ' "';
+            document.getElementById("author").innerHTML = "--  " + data.author;
+        },
+        error: function(err) { alert(err); },
+        beforeSend: function(xhr) {
+            xhr.setRequestHeader("X-Mashape-Authorization", "l75xvt8EmDmshqIBu665D14qnTOZp1FiQHDjsnm7HimMyWWOIS"); // Enter here your Mashape key
+        }
+    }); 
+}
 
 
 function randomColor() {
